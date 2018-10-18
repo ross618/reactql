773126f2d5b5efda967343926ba9a272f364b112
Ross <ross@sov.tech>
2018-10-18 15:48:31 +0200

    Updated styling

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
 
+//components
+import BookDetails from './BookDetails';
+
 class BookList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
   displayBooks(){
     var data = this.props.data;
     if(data.loading){
@@ -11,7 +20,7 @@ class BookList extends Component {
     else {
       return data.books.map(book => {
         return(
-          <li key={ book.id }>{ book.name }</li>
+          <li key={ book.id } onClick={ (e) => {this.setState({selected: book.id})} }>{ book.name }</li>
         );
       })
     }
@@ -23,6 +32,7 @@ class BookList extends Component {
       <ul id="book-list">
         { this.displayBooks() }
       </ul>
+      <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
